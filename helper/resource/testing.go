789db5ced3c1a7a292dfbfef8e50d46dfb1651b9
Alex Pilon <apilon@hashicorp.com>
2019-10-11 17:56:34 -0400

    add todos for missing aspects

--- helper/resource/testing.go
+++ helper/resource/testing.go
@@ -527,11 +527,6 @@ func Test(t TestT, c TestCase) {
 		c.PreCheck()
 	}
 
-	if acctest.TestHelper != nil {
-		RunLegacyTest(t.(*testing.T), c)
-		return
-	}
-
 	// get instances of all providers, so we can use the individual
 	// resources to shim the state during the tests.
 	providers := make(map[string]terraform.ResourceProvider)
@@ -541,6 +536,12 @@ func Test(t TestT, c TestCase) {
 			t.Fatal(err)
 		}
 		providers[name] = p
+	}
+
+	if acctest.TestHelper != nil {
+		// inject providers for ImportStateVerify
+		RunLegacyTest(t.(*testing.T), c, providers)
+		return
 	}
 
 	providerResolver, err := testProviderResolver(c)
